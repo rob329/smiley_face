b541b90fa3521a86fb356cd6cfd7ff84d16969c6
Robert Bolt <robertbolt@roberts-macbook-air.local>
2018-10-22 14:57:26 -0500

    Initial Commit

---
+++ smiley_faceTests/smiley_faceTests.swift
@@ -0,0 +1,36 @@
+//
+//  smiley_faceTests.swift
+//  smiley_faceTests
+//
+//  Created by Robert Bolt on 10/22/18.
+//  Copyright © 2018 Robert Bolt. All rights reserved.
+//
+
+import XCTest
+@testable import smiley_face
+
+class smiley_faceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
